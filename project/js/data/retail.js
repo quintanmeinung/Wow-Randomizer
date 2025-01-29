1d1788b774b8df0bb4234c0b17dadb3477a3c34c
quintanmeinung <quintanmeinung@gmail.com>
2025-01-28 19:47:48 -0800

    3rd Update.
    Now has a Cataclysm Randomizer!
    Updated the UI design.
    Included Worgen in the Retail Version.

--- project/js/data/retail.js
+++ project/js/data/retail.js
@@ -4,19 +4,20 @@ export const retailData = {
     genders: ["Male", "Female"],
     validCombinations : {
         Alliance: { 
-            Human: ["Warrior", "Mage", "Priest", "Warlock", "Rogue", "Monk", "Death_Knight"],
+            Human: ["Warrior", "Mage", "Priest", "Hunter", "Warlock", "Rogue", "Monk", "Death_Knight"],
             Dwarf: ["Warrior", "Hunter", "Paladin", "Priest", "Rogue", "Mage", "Shaman", "Warlock", "Monk", "Death_Knight"],
             Gnome: ["Warrior", "Mage", "Warlock", "Rogue", "Hunter", "Priest", "Death_Knight", "Monk"],
             Night_Elf: ["Warrior", "Druid", "Priest", "Hunter", "Rogue", "Mage", "Warlock", "Monk", "Death_Knight", "Demon_Hunter"],
             Draenei: ["Warrior", "Priest", "Hunter", "Rogue", "Mage", "Warlock", "Paladin", "Shaman", "Monk", "Death_Knight"],
-            Panderen_A: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Monk", "Death_Knight"],
-            Dracthyr_A: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Evoker"],
+            Worgen: ["Warrior", "Druid", "Priest", "Hunter", "Rogue", "Mage", "Monk", "Warlock", "Death_Knight"],
+            Panderen: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Monk", "Death_Knight"],
+            Dracthyr: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Evoker"],
             Void_Elf: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Death_Knight"],
             Lightforged_Draenei: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Paladin", "Death_Knight"],
             Dark_Iron_Dwarf: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Paladin", "Monk", "Death_Knight"],
             Kul_Tiran: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Shaman", "Death_Knight", "Druid"],
             Mechagnome: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Death_Knight"],
-            Earthen_H: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Paladin", "Shaman"]    
+            Earthen: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Paladin", "Shaman"]    
         },
         Horde: {
             Orc: ["Warrior", "Hunter", "Shaman", "Warlock", "Rogue", "Mage", "Priest", "Monk", "Death_Knight"],
@@ -25,14 +26,14 @@ export const retailData = {
             Undead: ["Warrior", "Mage", "Priest", "Hunter", "Warlock", "Rogue", "Monk", "Death_Knight"],
             Blood_Elf: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Paladin", "Demon_Hunter", "Monk", "Death_Knight"],
             Goblin: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Monk", "Death_Knight"],   
-            Panderen_H: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Monk", "Death_Knight"],      
-            Dracthyr_H: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Evoker"],        
+            Panderen: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Shaman", "Monk", "Death_Knight"],      
+            Dracthyr: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Evoker"],        
             Nightborne: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Death_Knight"],        
             High_Mountain_Tauren: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Death_Knight", "Druid", "Shaman"],         
             Maghar_Orc: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Shaman", "Death_Knight"],          
             Zandalari_Troll: ["Warrior", "Hunter", "Druid", "Shaman", "Paladin", "Priest", "Warlock", "Rogue", "Mage", "Monk", "Death_Knight"],   
             Vulpera: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Shaman", "Death_Knight"],
-            Earthen_A: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Paladin", "Shaman"]
+            Earthen: ["Warrior", "Mage", "Hunter", "Priest", "Warlock", "Rogue", "Monk", "Paladin", "Shaman"]
         }
     }
 };
